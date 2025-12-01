aa57e90145977c06ab7305a1eb4e4183b612bb14
halterman <halterman@southern.edu>
2025-12-01 13:29:41 -0500

    Added classify by word length dictionary example

--- 2025-11-21/countwords.py
+++ 2025-11-21/countwords.py
@@ -11,6 +11,18 @@ for word in words:
     else:
         counters[word] += 1
 
-for word, count in counters.items():
+for word, count in sorted(counters.items()):
     print(word, count)
+
+
+# # Sort by keys (that is, the words)
+# for word, count in sorted(counters.items()):
+#     print(word, count)
+
+# # Sort by values (that is, the counts)
+# counts: list[tuple[int, str]] = []
+# for word, count in sorted(counters.items()):
+#     counts.append((count, word))
+# for pair in sorted(counts):
+#     print(pair)
 
